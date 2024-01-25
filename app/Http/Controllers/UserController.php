583ed1ec8b82a1e493af95175d5c42461d476211
OsalexRod <alejandrorodriguezsalazar997@gmail.com>
2024-01-25 12:10:47 -0400

    probando Eloquent

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Show the profile for a given user.
+     */
+    public function show(string $id): View
+    {
+        return view('user.profile', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
+
+    public function index(): View
+    {
+        return view('user.index');
+    }
+
+    public function create(): View
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request)
+    {
+        User::
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Models\Usuario;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return 'Hello World 69';
+});
+
+Route::get('/usuarios', function () {
+    return Usuario::all();
+    //return response()->json(Usuario::all());
+});
+
+Route::get('/usuarios/{id}', function (string $id) {
+    return Usuario::findOrFail(intval($id));
+});
+
+Route::get('/usuarios/{nombre}/{correo}/{ci}', function (string $nombre, string $correo, string $ci) {
+    $usuarios = new Usuario;
+    $usuarios->nombre = $nombre;
+    $usuarios->correo = $correo;
+    $usuarios->ci = intval($ci);
+    $usuarios->save();
+    return "Correcto!!!";
 });
